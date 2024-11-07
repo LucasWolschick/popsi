63be3771072b0b16370ee164f170fa222cea023f
GuiSebax <guiclemente2003@hotmail.com>
2024-11-07 14:32:46 -0300

    continuacao

--- src/popsi/Parser.java
+++ src/popsi/Parser.java
@@ -71,6 +71,9 @@ public class Parser {
         consume(Token.TokenType.L_PAREN, "Esperado '(' após o nome da função");
         parseParameterList();
         consume(Token.TokenType.R_PAREN, "Esperado ')' após os parâmetros");
+        // if (match(Token.TokenType.ARROW)) {
+        // consume(Token.TokenType.IDENTIFIER, "Esperado tipo de retorno após '->'");
+        // }
         consume(Token.TokenType.L_CURLY, "Esperado '{' no início do corpo da função");
         parseBlock();
         consume(Token.TokenType.R_CURLY, "Esperado '}' no final do corpo da função");
@@ -97,12 +100,14 @@ public class Parser {
             parseVariableDeclaration();
         } else if (match(Token.TokenType.FOR)) {
             parseForStatement();
+        } else if (match(Token.TokenType.WHILE)) {
+            parseWhileStatement();
         } else if (match(Token.TokenType.IF)) {
             parseIfStatement();
         } else if (match(Token.TokenType.RETURN)) {
             parseReturnStatement();
-        } else if (match(Token.TokenType.WHILE)) {
-            parseWhileStatement();
+        } else if (match(Token.TokenType.DEBUG)) {
+            parseDebugStatement();
         } else {
             parseExpression();
         }
@@ -119,23 +124,54 @@ public class Parser {
     }
 
     private void parseForStatement() {
-        // parse pro for
+        consume(Token.TokenType.IDENTIFIER, "Esperado nome da variável do loop 'for'");
+        consume(Token.TokenType.COLON, "Esperado ':' após a variável do loop");
+        consume(Token.TokenType.IDENTIFIER, "Esperado tipo da variável do loop");
+
+        // consume(Token.TokenType.IN, "Esperado 'in' para o intervalo do loop");
+        parseRangeExpression();
+        parseBlock();
     }
 
     private void parseIfStatement() {
-        // Parse pro if
+        consume(Token.TokenType.L_PAREN, "Esperado '(' após 'if'");
+        parseExpression();
+        consume(Token.TokenType.R_PAREN, "Esperado ')' após a expressão do 'if'");
+        parseBlock();
+
+        // if (match(Token.TokenType.ELSE)) {
+        // if (match(Token.TokenType.IF)) {
+        // parseIfStatement(); // "else if"
+        // } else {
+        // parseBlock(); // "else" block
+        // }
+        // }
     }
 
     private void parseWhileStatement() {
-        // Parse pro while
+        consume(Token.TokenType.L_PAREN, "Esperado '(' após 'while'");
+        parseExpression();
+        consume(Token.TokenType.R_PAREN, "Esperado ')' após a expressão do 'while'");
+        parseBlock();
     }
 
     private void parseReturnStatement() {
         parseExpression();
-        consume(Token.TokenType.SEMICOLON, "Esperado ';' após a declaração return");
+        consume(Token.TokenType.SEMICOLON, "Esperado ';' após a declaração 'return'");
+    }
+
+    private void parseDebugStatement() {
+        parseExpression();
+        consume(Token.TokenType.SEMICOLON, "Esperado ';' após o comando 'debug'");
+    }
+
+    private void parseRangeExpression() {
+        consume(Token.TokenType.DOT_DOT, "Esperado '..' no intervalo");
+        consume(Token.TokenType.HASH, "Esperado '#' para denotar comprimento do vetor");
+        consume(Token.TokenType.IDENTIFIER, "Esperado nome do vetor após '#'");
     }
 
     private void parseExpression() {
-        // Implementar a análise de expressões, incluindo operadores e literais
+        // Implementação da análise de expressões, incluindo operadores e literais
     }
 }
